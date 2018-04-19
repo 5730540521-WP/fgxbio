54fa792e98e646b1529cae8b23830f043ba5f290
Wasin Panitansirikul <wasin.p1995@gmail.com>
2018-04-19 14:35:20 +0700

    Update Server API 19_4

--- server/src/api/admin.js
+++ server/src/api/admin.js
@@ -1,29 +1,78 @@
-const { Router } = require('express')
-const child_proc = require('child_process')
+const { Router } = require("express");
+const child_proc = require("child_process");
+const constant = require("../config/constant");
+const con = constant.con;
+const multer = require("multer");
 
-let router = Router()
+let router = Router();
 
-router.post('/adddata', (req, res, next) => {
-  child_proc.exec(`python ${__dirname}/`/*run generate script from excel */ , function(err) {
-    if(err) {
-      console.log("generate script fail with error : " + err)
-      res.status(400).send('error generating mySQL script');
+var execsql = require("execsql");
+var dbConfig = {
+  host: "localhost",
+  user: "root",
+  password: "Uq42=Tc8"
+};
+var sql = "use db_cam;";
+var sqlFile = "./SQLcommand.sql";
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function(req, file, cb) {
+    console.log(file.mimetype);
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+router.post("/exceladd", upload.single("file"), async (req, res, next) => {
+  if (req.file) {
+    console.log(`Received file ${req.file.originalname}`);
+    child_proc.exec(
+      `python ./python/GenAddQuerytry.py ./uploads/${req.file.originalname}`,
+      function(err) {
+        if (err) {
+          console.log("child process failed with error : " + err);
+          res.status(400).send("error");
+        } else {
+          console.log("SQL SCRIPT GENERATE");
+          execsql
+            .config(dbConfig)
+            .exec(sql)
+            .execFile(sqlFile, function(err, results) {
+              if (err) throw err;
+              console.log(results);
+              res.send("success");
+            });
+        }
+      } /*run generate script from excel */
+    );
+  }
+});
+
+router.post("/seqalign", async (req, res, next) => {
+  con.query(
+    `SELECT * FROM ngs_data WHERE Locus = '${req.body.locus}' && Allele = '${
+      req.body.allele
+    }';`,
+    function(err, rows) {
+      if (err) throw err;
+      child_proc.exec(
+        `python ./python/Sequence_Alignment.py ${req.body.locus} ${
+          req.body.allele
+        }`,
+        function(err, output) {
+          if (err) {
+            console.log("child process failed with error : " + err);
+            res.status(400).send("error");
+          }
+
+          console.log(output);
+          //console.log("PROCESS DONE");
+          res.send({ info: rows, pattern: output.split("|") });
+        }
+      );
     }
-    console.log('SQL SCRIPT GENERATED');
-    // run mySQL script
-  })
-})
+  );
+});
 
-
-router.post('/upload', (req, res, next) => {
-  //req.files.pic.mv(`${__dirname}`)
-  console.log(req.files.pic.name)
-  res.send('File Upload!')
-  console.log(`${__dirname}` + '/../')
-
-})
-router.get('/seqalign', (req, res, next) => {
-  
-})
-
-module.exports = router
+module.exports = router;
--- server/src/api/search.js
+++ server/src/api/search.js
@@ -1,8 +1,22 @@
 const { Router } = require("express");
 const _ = require("lodash");
+const fs = require("fs");
+const multer = require("multer");
 
 const authServ = require("../services/auth");
-const con = require("../config/constant");
+const constant = require("../config/constant");
+var path = require("path");
+
+const con = constant.con;
+
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function(req, file, cb) {
+    console.log(file.mimetype);
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
 
 let router = Router();
 
@@ -18,14 +32,21 @@ router.get("/", async (req, res, next) => {
 
 router.post("/manual", async (req, res, next) => {
   //req with a lot of param of [ {'locus': "locus", 'allele': "15" }, {} , {} ]
-  console.log(req.body);
   var data = req.body;
+  var minimal = [];
+  data.map(entry => {
+    for (i = 0; i < constant.Minimal_A.length; i++)
+      if (entry.locus == constant.Minimal_A[i]) minimal.push(entry);
+    for (i = 0; i < constant.Minimal_Y.length; i++)
+      if (entry.locus == constant.Minimal_Y[i]) minimal.push(entry);
+  });
+  //console.log("data", data);
+  //console.log("minimal", minimal);
   var command =
     "SELECT b.Sample_Year, b.Sample_ID FROM (SELECT a.Sample_Year, a.Sample_ID, COUNT(*) AS amount FROM (SELECT * FROM ngs_data WHERE ";
-  console.log(data.length);
+  //console.log(data.length);
   data.forEach(function(item) {
-    console.log(item.locus);
-    console.log(item.allele);
+    console.log(item.locus, " ", item.allele);
     command += `( Locus = '${item.locus}' && Allele = '${item.allele}') ||`;
   });
   command = command.substring(0, command.length - 3);
@@ -33,14 +54,43 @@ router.post("/manual", async (req, res, next) => {
     data.length
   };`;
   console.log(command);
-  con.query(command, function(err, rows) {
+  con.query(command, function(err, resultSearch) {
     if (err) throw err;
-    res.json(rows);
+    var command2 =
+      "SELECT b.Sample_Year, b.Sample_ID FROM (SELECT a.Sample_Year, a.Sample_ID, COUNT(*) AS amount FROM (SELECT * FROM ngs_data WHERE ";
+    minimal.forEach(function(item) {
+      console.log(item.locus, " ", item.allele);
+      command2 += `( Locus = '${item.locus}' && Allele = '${item.allele}') ||`;
+    });
+    command2 = command2.substring(0, command2.length - 3);
+    command2 += `) a GROUP BY a.Sample_Year, a.Sample_ID) b WHERE b.amount = ${
+      minimal.length
+    };`;
+    console.log(command2);
+    con.query(command2, function(err, resultExpect) {
+      if (err) throw err;
+      res.json({ result: resultSearch, expect: resultExpect });
+    });
   });
 });
 
-router.post("/excel", async (req, res, next) => {
+router.post("/excelsearch", upload.single("file"), async (req, res, next) => {
   //req with excel file
+  //console.log(req);
+  console.log(req.file);
+
+  if (req.file) {
+    console.log(`Received file ${req.file.originalname}`);
+    res.send("upload success");
+  }
+});
+
+router.get("/sampleexcel", (req, res, next) => {
+  res.download("./src/public/SampleExcel.xlsx", function(err) {
+    if (err) {
+      res.send(err);
+    }
+  });
 });
 
 router.get("/done", async (req, res, next) => {
